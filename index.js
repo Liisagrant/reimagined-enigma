35dfde397e71cfe86ac3085a2097790756a95e68
Liisagrant <lisagrant1990@gmail.com>
2022-06-09 13:56:11 +0200

    changed nav, and some styling

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+  myScroll();
+};
+
+const nav = document.querySelector("#navbar");
+const sticky = nav.offsetTop;
+
+function myScroll() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
+console.log("hello fellow friend, add me on github!");

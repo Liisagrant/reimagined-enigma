512dfff90a72df673a4853446652e3cc7d24e1b6
Liisagrant <lisagrant1990@gmail.com>
2022-06-09 22:38:24 +0200

    done a lot.
    First had a img slider. removed it.
    changed and made cards for portfolio.
    added about me and contact.
    added a img under contact

--- index.js
+++ index.js
@@ -13,17 +13,4 @@ function myScroll() {
   }
 }
 
-const nextImageDelay = 4000;
-let currentImgCounter = 0;
-const slideImages = document.querySelectorAll(".slideshow");
-
-const nextImage = () => {
-  slideImages[currentImgCounter].style.opacity = 0;
-  currentImgCounter = (currentImgCounter + 1) % slideImages.length;
-  slideImages[currentImgCounter].style.opacity = 1;
-};
-
-slideImages[currentImgCounter].style.opacity = 1;
-setInterval(nextImage, nextImageDelay);
-
 console.log("hello fellow friend, add me on github!");

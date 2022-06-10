088b100a3a356f7a7a22e6ef31dfcb8eb6e13c45
Liisagrant <lisagrant1990@gmail.com>
2022-06-10 10:36:39 +0200

    refactor and cleaning up

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 window.onscroll = function () {
-  myScroll();
+    myScroll();
 };
 
 const nav = document.querySelector("#navbar");
 const sticky = nav.offsetTop;
 
 function myScroll() {
-  if (window.pageYOffset >= sticky) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
+    if (window.pageYOffset >= sticky) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
 }
 
 console.log("hello fellow friend, add me on github!");
